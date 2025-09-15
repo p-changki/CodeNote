c4d752fdf81454f2b9953163e2f22c128e13bc82
p-changki <dnsxj12345aa@gmail.com>
2025-09-15 17:39:48 +0900

    feat : 연습3

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,61 @@
-export function App(){
-  return <p>하이</p>
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputText, setInputText] = useState("");
+
+  const handleAddTodo = () => {
+    if (inputText.trim() === "") return;
+    setTodos((prev) => [...prev, inputText]);
+    console.log("입력");
+    setInputText("");
+  };
+
+  //한글입력 중복방지
+  const handleKeyDown = (e) => {
+    if (e.nativeEvent.isComposing) return;
+    if (e.key === "Enter") {
+      handleAddTodo();
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="titleWrap">
+        <h1 className="title">TODOList</h1>
+        <p className="today">
+          오늘은 {new Date().toLocaleDateString()} 입니다.
+        </p>
+      </div>
+      <section className="inputSection">
+        <input
+          className="inputText"
+          type="text"
+          placeholder="여기에 적으세요!"
+          value={inputText}
+          onChange={(e) => {
+            setInputText(e.target.value);
+          }}
+          onKeyDown={handleKeyDown}
+        ></input>
+        <button className="inputBtn" onClick={handleAddTodo}>
+          확인
+        </button>
+      </section>
+      <section className="todoSection">
+        {todos.length === 0 ? (
+          <p> 투두리스트를 입력한게 들어와요!</p>
+        ) : (
+          <ul className="todoList">
+            {todos.map((todo, index) => (
+              <li key={index}>{todo}</li>
+            ))}
+          </ul>
+        )}
+      </section>
+    </div>
+  );
 }
+
+export default App;
