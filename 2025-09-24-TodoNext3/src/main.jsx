03cf8f090699b088f4993a4dda41a1e614674707
p-changki <dnsxj12345aa@gmail.com>
2025-09-25 11:50:54 +0900

    feat : 경로 설정 (jsconfig.json)

--- 2025-09-24-TodoNext3/src/main.jsx
+++ 2025-09-24-TodoNext3/src/main.jsx
@@ -2,7 +2,7 @@ import App from "./App.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { ThemeProvider } from "./providers/ThemeContext.jsx";
+import { ThemeProvider } from "@/providers";
 import "./theme.css";
 
 createRoot(document.getElementById("root")).render(
